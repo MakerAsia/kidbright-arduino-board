9c82343e96143c6ba503f2e7c2e62d964dda8191
bavensky <apiruk_sang-ngenchai@hotmail.com>
2019-06-17 17:56:40 +0700

    change link

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ module.exports = {
   author: "Maker Asia",
   website: "http://www.kbpro.org",
   email: "makerasia@gmail.com",
-  git: "https://github.com/makerasia/kbpro-board/",
+  git: "https://github.com/MakerAsia/kidbright-arduino-board/",
   image: "/static/display.jpg",
   version: "1.0.1",
 };
